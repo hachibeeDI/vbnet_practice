911dbdd4bd4f709e5407b22314bcb2fe8271a4cc
ogura-remote <ogura@aimu.co.jp>
2012-10-29 17:11:13 +0900

    schedulerコントロールのイベント処理を委譲するためのクラスを定義

---
+++ Book/Forms/ViewController/SchedulerDataDispatcher.vb
@@ -0,0 +1,37 @@
+﻿'
+' Written by : OGURA_Daiki
+'    Date    : 2012-10-29
+'   Remarks  :
+'
+Option Strict On
+Option Infer On
+Option Explicit On
+
+Imports DevExpress
+
+Namespace Forms.ViewController
+    Public Class SchedulerDataDispatcher
+
+        Private WithEvents _scheduler As DevExpress.XtraScheduler.SchedulerControl
+        Private ReadOnly Property scheduler As XtraScheduler.SchedulerControl
+            Get
+                Return _scheduler
+            End Get
+        End Property
+
+        Private WithEvents _scheduler_storage As DevExpress.XtraScheduler.SchedulerStorage
+        Private ReadOnly Property scheduler_storage As XtraScheduler.SchedulerStorage
+            Get
+                Return _scheduler_storage
+            End Get
+        End Property
+
+        Public Sub New(scheduler_control As XtraScheduler.SchedulerControl)
+            _scheduler = scheduler_control
+            _scheduler_storage = _scheduler.Storage
+        End Sub
+
+
+
+    End Class
+End Namespace
