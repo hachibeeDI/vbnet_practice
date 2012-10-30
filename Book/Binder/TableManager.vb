215d61e9b6dff433c476b61cfcf4b639226687c7
ogura-remote <ogura@aimu.co.jp>
2012-10-30 17:45:24 +0900

    テーブルid追加時の例外処理を委譲させた

--- Book/Binder/TableManager.vb
+++ Book/Binder/TableManager.vb
@@ -7,9 +7,10 @@ Option Strict On
 Option Infer On
 Option Explicit On
 
+Imports MyADOHelper.ExceptionLogic
+Imports MyADOHelper.ExceptionLogic.ErrorState
+Imports MyADOHelper.Helper
 
-
-Imports MyADOHelper
 Imports Book.Utils.Message
 
 Namespace Binder
@@ -21,23 +22,16 @@ Namespace Binder
     Public Class TableManager
         Inherits TableController
 
-        Public Overrides Function addTable(table_id As Integer, maximum_seat As Integer) As Integer
+        Public Shadows Function addTable(table_id As Integer, maximum_seat As Integer) As IErrorState
             Dim tables = MyBase.getAllTables()
             If tables.Where(Function(t) t.id = table_id).Any Then
-                '主キーの重複
-                CustomMessage.showInfo(SITUATIONS.無効な入力値.ToMessageText, "Info")
-                Return 0 : End If
+                Return New ErrorState.PrimarykeyOverlap : End If
 
-            Try
-                Return MyBase.addTable(table_id, maximum_seat)
-            Catch e As SqlClient.SqlException
-                writeLog("ERROR", e.Message)
-                '例えば、エラーコードによってはそのまま処理したい？　その場合はまた別の拡張を考える。
-                'EnumのROR_CODEはPublicとなっている
-                Dim m = New ExceptionLogic.Message(e.Number)
-                CustomMessage.showError(m.getMessageByNumber, "エラー")
-                Return 0
-            End Try
+            Dim insert = Functional.pertial(AddressOf MyBase.addTable,
+                                            table_id, maximum_seat)
+
+            Return TryADOExcute.catchSQLState(insert)
+
         End Function
 
     End Class
