e9ca47fd2313fd3926c76b03f564b59b52c024f0
ogura-remote <ogura@aimu.co.jp>
2012-10-30 14:22:39 +0900

    SQLExせpちおnをラップするExceptionを追加

---
+++ MyADOHelper/Error/Exceptions/MyException.vb
@@ -0,0 +1,20 @@
+﻿'
+' Written by : OGURA_Daiki
+'    Date    : 2012-10-23
+'   Remarks  :
+'
+Option Strict On
+Option Infer On
+Option Explicit On
+
+Namespace ExceptionLogic.Exceptions
+
+    Public Class InvailedSQLStatusException
+        Inherits Exception
+
+        Public Sub New(message As String)
+            MyBase.New(message)
+        End Sub
+
+    End Class
+End Namespace
