a7b68cd48f2a3be4503a8f317fd3fa29068954ac
ogura-remote <ogura@aimu.co.jp>
2012-10-30 13:40:29 +0900

    ひな形咲くせい

--- Book/Binder/BookingManager.vb
+++ Book/Binder/BookingManager.vb
@@ -11,21 +11,31 @@ Option Explicit On
 Imports MyADOHelper
 
 Imports Book.Utils.Message
+Imports Book.Utils.Extension.DateTimeExtension
+
 
 Namespace Binder
 
     Public Class BookingManager
         Inherits BookingController
 
+        Private _all_tables As IEnumerable(Of Models.Tables)
+        Private ReadOnly Property all_tables As IEnumerable(Of Models.Tables)
+            Get
+                If _all_tables Is Nothing Then
+                    _all_tables = New TableManager().getAllTables : End If
+                Return _all_tables
+            End Get
+        End Property
+
         Public Sub New()
         End Sub
 
-
-        Public Overrides Function _
+        Public Shadows Function _
         reserveBooking(table_id As Integer, start As DateTime, close As DateTime, numberof_seats As Integer, name As String) _
         As Integer
             'このチェックいらないかも
-            Dim ids = New TableManager().getAllTables.Select(Function(t) t.id)
+            Dim ids = all_tables.Select(Function(t) t.id)
             If Not ids.Any(Function(id) id = table_id) Then
                 CustomMessage.showError(SITUATIONS.存在しないid.ToMessageText, "エラー")
                 Return 0
@@ -46,6 +56,30 @@ Namespace Binder
                 CustomMessage.showError(e.Message, "エラー")
                 Return 0
             End Try
+
+        End Function
+
+
+        Public Shadows Function _
+        alterBooking(booking_id As UInteger, table_id As Integer, start As DateTime, close As DateTime,
+                    Optional numberof_persons As Integer = 0, Optional name As String = Nothing) _
+        As Integer
+
+            If beforeAlterValidate() Then
+
+            End If
+
+            Return MyBase.alterBooking(Convert.ToInt32(booking_id), table_id, start, close, numberof_persons, name)
+
+        End Function
+        Private Function _
+        beforeAlterValidate(table_id As Integer, start As DateTime, close As DateTime) _
+        As Boolean
+
+            If Not start.isLessThen(close) Then Return False
+
+
+            Dim today = Date.Today
 
         End Function
 
