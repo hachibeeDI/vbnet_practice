2bcda15aa4de58755824e79d1b66d3d4c5595838
ogura-remote <ogura@aimu.co.jp>
2012-10-30 15:28:46 +0900

    コメントとテスト用実装の追加

--- MyADOHelper/Helper/SysLog.vb
+++ MyADOHelper/Helper/SysLog.vb
@@ -12,12 +12,14 @@ Module SysLog
     ''' <summary>
     ''' ログファイルへの書き込みを行う
     ''' </summary>
-    ''' <param name="prefix"></param>
-    ''' <param name="message"></param>
-    ''' <remarks></remarks>
+    ''' <param name="prefix">logをgrepするときのフィルターとして使ったり、意味で分けたりする</param>
+    ''' <param name="message">メッセージ本体</param>
+    ''' <remarks>ログの書式は統一すること</remarks>
     Public Sub writeLog(prefix As String, message As String)
         Dim content = String.Format("{0} : Log = {1}", prefix, message)
         'ログ出力の処理はここに集約
-
+        'とりあえずは標準出力に流す
+        Debug.WriteLine(content)
     End Sub
+
 End Module
