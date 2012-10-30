21aabeb5c5dea5759ccc41a7dde5b24f97ce34c1
ogura-remote <ogura@aimu.co.jp>
2012-10-30 11:31:30 +0900

    予約取得と、予約更新のオーバーロードを作成

--- Book/Binder/BookingController.vb
+++ Book/Binder/BookingController.vb
@@ -30,10 +30,12 @@ Namespace Binder
         Protected Sub New()
         End Sub
 
+        ''' <summary> すべての予約を取得 </summary>
         Public Overridable Function getAllBookings() As IEnumerable(Of Models.Bookings)
             Return _
             DbExecutor.ExecuteReader(MyBase.getConnection,
-                <sql>SELECT TB.tables_id,
+                <sql>SELECT TB.id,
+                           TB.tables_id,
                            TB.starting_time,
                            TB.closing_time,
                            TB.name_haveReservation,
@@ -44,21 +46,24 @@ Namespace Binder
                     Select(
                     Function(row) New Models.Bookings() With
                                      {
+                                         .id = row.getValue_Type(Of Integer)("id"),
                                          .tables_id = row.getValue_Type(Of Integer)("tables_id"),
                                          .starting_time = row.getValue_Type(Of DateTime)("starting_time"),
                                          .closing_time = row.getValue_Type(Of DateTime)("closing_time"),
                                          .name_haveReservation = row.getValue_Type(Of String)("name_haveReservation"),
-            .num_persons = row.getValue_Type(Of Integer)("num_persons")
+            .numberof_persons = row.getValue_Type(Of Integer)("num_persons")
                                      }
                                  ).ToArray()
         End Function
 
+        ''' <summary> 指定日の予約を取得 </summary>
         Public Overridable Function getBookings(targ_date As DateTime) As IEnumerable(Of Models.Bookings)
             'Dynamicの方を使えばIDataRecordからの型変換はいらないけども……
             'Dynamicも十分高速。移行を検討
             Return _
             DbExecutor.ExecuteReader(MyBase.getConnection,
-                <sql>SELECT TB.tables_id,
+                <sql>SELECT TB.id,
+                           TB.tables_id,
                            TB.starting_time,
                            TB.closing_time,
                            TB.name_haveReservation,
@@ -70,14 +75,40 @@ Namespace Binder
                     Select(
                     Function(row) New Models.Bookings() With
                                      {
+                                         .id = row.getValue_Type(Of Integer)("id"),
                                          .tables_id = row.getValue_Type(Of Integer)("tables_id"),
                                          .starting_time = row.getValue_Type(Of DateTime)("starting_time"),
                                          .closing_time = row.getValue_Type(Of DateTime)("closing_time"),
                                          .name_haveReservation = row.getValue_Type(Of String)("name_haveReservation"),
-                                         .num_persons = row.getValue_Type(Of Integer)("num_persons")
+                                         .numberof_persons = row.getValue_Type(Of Integer)("num_persons")
                                      }
                                  ).ToArray()
         End Function
+
+        ''' <summary> 指定日の予約を取得 </summary>
+        Public Overridable Function getBookings(targ_id As Integer) As IEnumerable(Of Models.Bookings)
+            'VBの構文解析がくさってて、一時分割しないとコンパイル時にエラー吐いたりするのでここは分割
+            Dim tmp =
+            DbExecutor.ExecuteReader(MyBase.getConnection,
+                <sql>SELECT
+                           *
+                       FROM Bookings
+                       WHERE id = @booking_id</sql>.Value,
+                    New With {.booking_id = targ_id})
+            Return tmp.
+                Select(
+                    Function(row)
+                        Return New Models.Bookings() With {
+                                .id = row.getValue_Type(Of Integer)("id"),
+                                .tables_id = row.getValue_Type(Of Integer)("tables_id"),
+                                .starting_time = row.getValue_Type(Of DateTime)("starting_time"),
+                                .closing_time = row.getValue_Type(Of DateTime)("closing_time"),
+                                .name_haveReservation = row.getValue_Type(Of String)("name_haveReservation"),
+                                .numberof_persons = row.getValue_Type(Of Integer)("num_persons")}
+                    End Function).
+                ToArray()
+        End Function
+
 
 
         ''' <summary> 新規に予約を取る </summary>
@@ -103,66 +134,119 @@ VALUES(
                                   .starting_time = start,
                                   .closing_time = close,
                                   .name_haveReservation = name,
-                                  .num_persons = numberof_seats
+                                  .numberof_persons = numberof_seats
                              })
         End Function
 
 
-        'Update部分は後日修正
+        ' ''' <summary> 予約を変更する </summary>
+        ' ''' <param name="table_id">対象のテーブル番号</param>
+        ' ''' <param name="start">予約の開始時刻</param>
+        ' ''' <param name="close">予約の終了時刻</param>
+        ' ''' <returns>引数は更新したい{開始時刻、終了時刻} 返り値は更新に成功した行数を返す</returns>
+        ' ''' <remarks>高階関数</remarks>
+        'Protected Overridable Overloads Function _
+        'alterBooking(booking_id As Integer, table_id As Integer,
+        '            start As DateTime, close As DateTime, numberof_persons As Integer) _
+        'As Func(Of Integer, Integer, Integer)
+
+        '    Return Function(new_start As Integer, new_close As Integer)
+        '               Return DbExecutor.Update(MyBase.getConnection,
+        '                                       "Bookings",
+        '                                       New With {
+        '                                           .starting_time = new_start.ToString,
+        '                                           .closing_time = new_close.ToString,
+        '                                           .num_persons = numberof_persons
+        '                                         },
+        '                                       New With {
+        '                                            .id = booking_id
+        '                                         }
+        '                                      )
+        '           End Function
+        'End Function
 
         ''' <summary> 予約を変更する </summary>
+        ''' <param name="booking_id">予約番号</param>
         ''' <param name="table_id">対象のテーブル番号</param>
-        ''' <param name="start">予約の開始時刻</param>
-        ''' <param name="close">予約の終了時刻</param>
-        ''' <returns>引数は更新したい{開始時刻、終了時刻} 返り値は更新に成功した行数を返す</returns>
-        ''' <remarks>高階関数</remarks>
+        ''' <param name="new_start">新規の開始時刻</param>
+        ''' <param name="new_close">新規の終了時刻</param>
+        ''' <param name="new_name">予約者の名前</param>
+        ''' <returns>引数は更新したい{開始時刻、終了時刻、氏名} 返り値は更新に成功した行数を返す</returns>
+        ''' <remarks></remarks>
         Protected Overridable Overloads Function _
         alterBooking(booking_id As Integer, table_id As Integer,
-                    start As DateTime, close As DateTime, numberof_persons As Integer) _
-        As Func(Of Integer, Integer, Integer)
+                     new_start As DateTime, new_close As DateTime, numof_persons As Integer, new_name As String) _
+        As Integer
 
-            Return Function(new_start As Integer, new_close As Integer)
-                       Return DbExecutor.Update(MyBase.getConnection,
+            Return DbExecutor.Update(MyBase.getConnection,
                                                "Bookings",
                                                New With {
+                                                   .table_id = table_id,
                                                    .starting_time = new_start.ToString,
                                                    .closing_time = new_close.ToString,
-                                                   .num_persons = numberof_persons
+                                                   .num_persons = numof_persons,
+                                                    .name_haveReservation = new_name
                                                  },
                                                New With {
                                                     .id = booking_id
                                                  }
-                                              )
-                   End Function
+                                             )
         End Function
 
         ''' <summary> 予約を変更する </summary>
+        ''' <param name="booking_id">予約番号</param>
         ''' <param name="table_id">対象のテーブル番号</param>
-        ''' <param name="start">予約の開始時刻</param>
-        ''' <param name="close">予約の終了時刻</param>
-        ''' <param name="name">予約者の名前</param>
+        ''' <param name="new_start">新規の開始時刻</param>
+        ''' <param name="new_close">新規の終了時刻</param>
         ''' <returns>引数は更新したい{開始時刻、終了時刻、氏名} 返り値は更新に成功した行数を返す</returns>
-        ''' <remarks>高階関数</remarks>
+        ''' <remarks></remarks>
         Protected Overridable Overloads Function _
         alterBooking(booking_id As Integer, table_id As Integer,
-                     start As DateTime, close As DateTime, numberof_persons As Integer, name As String) _
-        As Func(Of DateTime, DateTime, String, Integer)
+                     new_start As DateTime, new_close As DateTime, numof_persons As Integer) _
+        As Integer
 
-            Return Function(new_start As DateTime, new_close As DateTime, new_name As String)
-                       Return DbExecutor.Update(MyBase.getConnection,
+            Return DbExecutor.Update(MyBase.getConnection,
                                                "Bookings",
                                                New With {
+                                                   .table_id = table_id,
                                                    .starting_time = new_start.ToString,
                                                    .closing_time = new_close.ToString,
-                                                   .num_persons = numberof_persons,
-                                                   .name_haveReservation = new_name
+                                                   .num_persons = numof_persons
                                                  },
                                                New With {
-                                                   .id = booking_id
+                                                    .id = booking_id
                                                  }
-                                              )
-                   End Function
+                                             )
         End Function
+
+
+
+        ''' <summary> 予約を変更する </summary>
+        ''' <param name="booking_id">予約番号</param>
+        ''' <param name="table_id">対象のテーブル番号</param>
+        ''' <param name="new_start">新規の開始時刻</param>
+        ''' <param name="new_close">新規の終了時刻</param>
+        ''' <returns>引数は更新したい{開始時刻、終了時刻、氏名} 返り値は更新に成功した行数を返す</returns>
+        ''' <remarks></remarks>
+        Protected Overridable Overloads Function _
+        alterBooking(booking_id As Integer, table_id As Integer,
+                     new_start As DateTime, new_close As DateTime) _
+        As Integer
+
+            Return DbExecutor.Update(MyBase.getConnection,
+                                               "Bookings",
+                                               New With {
+                                                   .table_id = table_id,
+                                                   .starting_time = new_start.ToString,
+                                                   .closing_time = new_close.ToString
+                                                 },
+                                               New With {
+                                                    .id = booking_id
+                                                 }
+                                             )
+        End Function
+
+
 
         ''' <summary>  </summary>
         ''' <param name="table_id"></param>
