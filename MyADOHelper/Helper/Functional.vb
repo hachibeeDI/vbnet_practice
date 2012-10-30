2d50eb7a800b22eefc72b36163d5d977a37682a5
ogura-remote <ogura@aimu.co.jp>
2012-10-30 15:28:26 +0900

    SQL発行時の例外集約用のクラスと、それを補助するための部分適用的モジュールを追加

---
+++ MyADOHelper/Helper/Functional.vb
@@ -0,0 +1,70 @@
+﻿'
+' Written by : OGURA_Daiki
+'    Date    : 2012-10-30
+'   Remarks  :
+'
+Option Strict On
+Option Infer On
+Option Explicit On
+
+Namespace Helper
+
+    Public Class Functional
+
+        Public Shared Function _
+        pertial(Of T1, TResult)(f As Func(Of T1, TResult), p1 As T1) _
+        As Func(Of TResult)
+
+            Return Function()
+                       Return f(p1)
+                   End Function
+        End Function
+
+        Public Shared Function _
+        pertial(Of T1, T2, TResult)(f As Func(Of T1, T2, TResult), p1 As T1, p2 As T2) _
+        As Func(Of TResult)
+
+            Return Function()
+                       Return f(p1, p2)
+                   End Function
+        End Function
+
+        Public Shared Function _
+        pertial(Of T1, T2, T3, TResult)(f As Func(Of T1, T2, T3, TResult), p1 As T1, p2 As T2, p3 As T3) _
+        As Func(Of TResult)
+
+            Return Function()
+                       Return f(p1, p2, p3)
+                   End Function
+        End Function
+
+        Public Shared Function _
+        pertial(Of T1, T2, T3, T4, TResult)(f As Func(Of T1, T2, T3, T4, TResult), p1 As T1, p2 As T2, p3 As T3, p4 As T4) _
+        As Func(Of TResult)
+
+            Return Function()
+                       Return f(p1, p2, p3, p4)
+                   End Function
+        End Function
+
+        Public Shared Function _
+        pertial(Of T1, T2, T3, T4, T5, TResult)(f As Func(Of T1, T2, T3, T4, T5, TResult), p1 As T1, p2 As T2, p3 As T3, p4 As T4, p5 As T5) _
+        As Func(Of TResult)
+
+            Return Function()
+                       Return f(p1, p2, p3, p4, p5)
+                   End Function
+        End Function
+
+        Public Shared Function _
+        pertial(Of T1, T2, T3, T4, T5, T6, TResult)(f As Func(Of T1, T2, T3, T4, T5, T6, TResult), p1 As T1, p2 As T2, p3 As T3, p4 As T4, p5 As T5, p6 As T6) _
+        As Func(Of TResult)
+
+            Return Function()
+                       Return f(p1, p2, p3, p4, p5, p6)
+                   End Function
+        End Function
+
+    End Class
+
+End Namespace
---
+++ MyADOHelper/Helper/TryADOExcute.vb
@@ -0,0 +1,39 @@
+﻿'
+' Written by : OGURA_Daiki
+'    Date    : 2012-10-30
+'   Remarks  :
+'
+Option Strict On
+Option Infer On
+Option Explicit On
+
+
+Namespace Helper
+
+    ''' <summary> 例外集約用クラス </summary>
+    ''' <remarks></remarks>
+    Public Class TryADOExcute
+
+        ''' <summary> SQLException時のログ取り処理などを集約する </summary>
+        ''' <param name="f"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Shared Function catchSQLState(f As Func(Of Integer)) As ExceptionLogic.ErrorState.IErrorState
+            Try
+                f()
+                Return New ExceptionLogic.ErrorState.NoError
+
+            Catch e As SqlClient.SqlException
+                writeLog("ERROR", e.Message & Environment.NewLine & e.StackTrace)
+
+                Return ExceptionLogic.Message.getState(e.Number)
+            Catch e As SqlTypes.SqlTypeException
+                writeLog("ERROR", e.Message)
+                Return New ExceptionLogic.ErrorState.UnknownError
+            End Try
+        End Function
+
+
+
+    End Class
+End Namespace
